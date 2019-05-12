bb369df85e3f9a47b3f0b5cc3dab0b59b0f9b0ad
Gavin <787549184@qq.com>
2019-05-13 00:44:10 +0800

    Redis缓存优化

---
+++ src/main/java/com/zz/seckill/controller/SeckillController.java
@@ -0,0 +1,105 @@
+package com.zz.seckill.controller;
+
+import com.zz.seckill.bean.Seckill;
+import com.zz.seckill.dto.Exposer;
+import com.zz.seckill.dto.SeckillExecution;
+import com.zz.seckill.dto.SeckillResult;
+import com.zz.seckill.enums.SeckillStatEnum;
+import com.zz.seckill.exception.RepeatKillException;
+import com.zz.seckill.exception.SeckillCloseException;
+import com.zz.seckill.exception.SeckillException;
+import com.zz.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+
+    @Autowired
+    private SeckillService seckillService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @ResponseBody
+    @RequestMapping("/findAll")
+    public List<Seckill> findAll() {
+        return seckillService.findAll();
+    }
+    
+    @ResponseBody
+    @RequestMapping("/findById")
+    public Seckill findById(@RequestParam("id") Long id) {
+        return seckillService.findById(id);
+    }
+    
+    @RequestMapping("/{seckillId}/detail")
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
+        if (seckillId == null) {
+            return "page/seckill";
+        }
+        Seckill seckill = seckillService.findById(seckillId);
+        model.addAttribute("seckill", seckill);
+        if (seckill == null) {
+            return "page/seckill";
+        }
+        return "page/seckill_detail";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    public SeckillResult<Exposer> exposer(@PathVariable("seckillId") Long seckillId) {
+        SeckillResult<Exposer> result;
+        try {
+            Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true, exposer);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result = new SeckillResult<Exposer>(false, e.getMessage());
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId,
+                                                   @PathVariable("md5") String md5,
+                                                   @RequestParam("money") BigDecimal money,
+                                                   @CookieValue(value = "killPhone", required = false) Long userPhone) {
+        if (userPhone == null) {
+            return new SeckillResult<SeckillExecution>(false, "未注册");
+        }
+        try {
+            SeckillExecution execution = seckillService.executeSeckill(seckillId, money, userPhone, md5);
+            return new SeckillResult<SeckillExecution>(true, execution);
+        } catch (RepeatKillException e) {
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(true, seckillExecution);
+        } catch (SeckillCloseException e) {
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.END);
+            return new SeckillResult<SeckillExecution>(true, seckillExecution);
+        } catch (SeckillException e) {
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.INNER_ERROR);
+            return new SeckillResult<SeckillExecution>(true, seckillExecution);
+        }
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/time/now")
+    public SeckillResult<Long> time() {
+        Date now = new Date();
+        return new SeckillResult(true, now.getTime());
+    }
+}
--- src/main/java/com/zz/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/com/zz/seckill/service/impl/SeckillServiceImpl.java
@@ -4,6 +4,7 @@ import com.zz.seckill.bean.Seckill;
 import com.zz.seckill.bean.SeckillOrder;
 import com.zz.seckill.dto.Exposer;
 import com.zz.seckill.dto.SeckillExecution;
+import com.zz.seckill.enums.SeckillStatEnum;
 import com.zz.seckill.exception.RepeatKillException;
 import com.zz.seckill.exception.SeckillCloseException;
 import com.zz.seckill.exception.SeckillException;
@@ -12,6 +13,8 @@ import com.zz.seckill.mapper.SeckillOrderMapper;
 import com.zz.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
@@ -23,16 +26,22 @@ import java.util.List;
 
 @Service
 public class SeckillServiceImpl implements SeckillService {
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     //设置盐值字符串，随便定义，用于混淆MD5值
     private final String salt = "sjajaspu-i-2jrfm;sd";
+    //设置秒杀redis缓存的key
+    private final String key = "seckill";
 
     @Resource
     private SeckillMapper seckillMapper;
 
     @Resource
     private SeckillOrderMapper seckillOrderMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public List<Seckill> findAll() {
@@ -59,10 +68,21 @@ public class SeckillServiceImpl implements SeckillService {
 
     @Override
     public Exposer exportSeckillUrl(long seckillId) {
-        Seckill seckill = seckillMapper.selectByPrimaryKey(seckillId);
+        Seckill seckill = (Seckill) redisTemplate.boundHashOps(key).get(seckillId);
         if (seckill == null) {
-            //说明没有查询到
-            return new Exposer(false, seckillId);
+            //说明redis缓存中没有此key对应的value
+            //查询数据库，并将数据放入缓存中
+            seckill = seckillMapper.selectByPrimaryKey(seckillId);
+            if (seckill == null) {
+                //说明没有查询到
+                return new Exposer(false, seckillId);
+            } else {
+                //查询到了，存入redis缓存中。 key:秒杀表的ID值； value:秒杀表数据
+                redisTemplate.boundHashOps(key).put(seckill.getSeckillId(), seckill);
+                logger.info("RedisTemplate -> 从数据库中读取并放入缓存中");
+            }
+        } else {
+            logger.info("RedisTemplate -> 从缓存中读取");
         }
         Date startTime = seckill.getStartTime();
         Date endTime = seckill.getEndTime();
@@ -83,6 +103,15 @@ public class SeckillServiceImpl implements SeckillService {
         return md5;
     }
 
+
+    /**
+     * 使用注解式事务方法的有优点：开发团队达成了一致约定，明确标注事务方法的编程风格
+     * 使用事务控制需要注意：
+     * 1.保证事务方法的执行时间尽可能短，不要穿插其他网络操作PRC/HTTP请求（可以将这些请求剥离出来）
+     * 2.不是所有的方法都需要事务控制，如只有一条修改的操作、只读操作等是不需要进行事务控制的
+     * <p>
+     * Spring默认只对运行期异常进行事务的回滚操作，对于编译异常Spring是不进行回滚的，所以对于需要进行事务控制的方法尽可能将可能抛出的异常都转换成运行期异常
+     */
     @Override
     @Transactional
     public SeckillExecution executeSeckill(long seckillId, BigDecimal money, long userPhone, String md5)
